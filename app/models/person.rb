a9fa3159f68e9796470bb82d230bf3b45ba91da3
Mike Land <mikepland@gmail.com>
2013-03-09 18:26:10 -0600

    create app and generate scaffold for People and Families

---
+++ app/models/person.rb
@@ -0,0 +1,3 @@
+class Person < ActiveRecord::Base
+  attr_accessible :family_id, :first_name, :last_name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExampleOfRailsFormWithNestedAttributes::Application.config.secret_token = 'c5a44973595268d7243dc60920cd8f630c54b6b9eddbd9d72b24bfc9f6aeffcc811b086c7eef6cec1fff436c9e12bb5c4fb608e2c3b458b60d0f5bd8fec46e43'
