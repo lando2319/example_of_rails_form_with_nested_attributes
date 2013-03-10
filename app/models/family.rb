38a236ab9095d7602f4660ff866fbcaed57b2a0f
Mike Land <mikepland@gmail.com>
2013-03-09 18:56:53 -0600

    add attribute code to the Family model

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,7 +1,9 @@
 class Family < ActiveRecord::Base
-  attr_accessible :family_name
+  attr_accessible :family_name, :people_attributes
 
   has_many :people
+
+  accepts_nested_attributes_for :people
 
 
 end
