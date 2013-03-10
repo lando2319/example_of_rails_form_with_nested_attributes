6d7fddd7256351b2eb46c81fe7a0b0ef8df05f1f
Mike Land <mikepland@gmail.com>
2013-03-09 18:34:14 -0600

    setup model association

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,3 +1,7 @@
 class Family < ActiveRecord::Base
   attr_accessible :family_name
+
+  has_many :people
+
+
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,6 @@
 class Person < ActiveRecord::Base
   attr_accessible :family_id, :first_name, :last_name
+
+  belongs_to :family
+
 end
